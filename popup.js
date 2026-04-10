04ff3c90f5e5b7508db67639fcf8b4dfe7861a06
ctct-proservices <proservices@constantcontact.com>
2026-04-10 14:33:46 -0700

    Add files via upload

--- popup.js
+++ popup.js
@@ -97,15 +97,25 @@
         modal.className = "cc-modal";
   
         modal.innerHTML = `
-          <div class="cc-modal-content">
-            <div class="cc-left">
-              <img src="${this.config.image}" />
-            </div>
-            <div class="cc-right">
-              <span class="cc-close">&times;</span>
-              <div id="cc-form"></div>
-            </div>
-          </div>
+          <div class="cc-modal-content" style="
+          display: flex;
+          flex-direction: ${imagePosition};
+          max-width:800px;
+          margin: 10% auto;
+          background: ${styles.backgroundColor || "#fff"};
+          border-radius: ${styles.borderRadius || "12px"};
+          overflow: hidden;
+          ">
+          <div class="cc-left" style="width: ${imageWidth}">
+          <img src="${this.config.image}" style="width:100%; height:100%; object-fit:cover;" />
+        </div>
+  
+        <div class="cc-right" style="width: ${formWidth}; padding: 20px;">
+          <span class="cc-close" style="float:right; cursor:pointer;">&times;</span>
+          <div id="cc-form"></div>
+        </div>
+  
+      </div>
         `;
   
         document.body.appendChild(modal);
